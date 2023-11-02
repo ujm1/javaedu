443c40e4232bc8ede95b43a8d2f8646c3309482d
jaman <woakswoaks@gmail.com>
2023-11-02 17:32:02 +0900

    자바실질코딩첫날-별찍기

--- src/edu/java/Exam7.java
+++ src/edu/java/Exam7.java
@@ -2,20 +2,36 @@ package edu.java;
 
 public class Exam7 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 2; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+
+            for (int l = 1; l < i; l++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
 
-        for (int k = 3; k <= 5; k++) {
-            for (int q = 0; q <= 5 - k; q++) {
+        for (int a = 0; a <= 1; a++) {
+
+
+            for (int b = 0; b <= a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 0; c < 1-a; c++) {
+                System.out.print("*");
+            }
+
+            for (int d = 0; d <= 1-a; d++) {
                 System.out.print("*");
             }
             System.out.println("");
+
         }
     }
+
 }
-
-
